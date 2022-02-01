74030adfb3873deb434a587aca7286c931986288
Owais Khan <45107209+kowais915@users.noreply.github.com>
2022-02-02 02:13:55 +0500

    hero is done

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
    <>
     <Nav/>
+    <Main/>
   
    
    </>
--- airbnb/src/components/Main.js
+++ airbnb/src/components/Main.js
@@ -1,10 +1,14 @@
+import grid from "../images/grid.png"
 const Main = () => {
     return (
-        <>
-        
-        <h2>Another test comoponent</h2>
-        
-        </>
+       <main>
+           <img className = "grid" src={grid}/>
+           <div className="title">
+               <h1>Online Experiences</h1>
+               <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+           </div>
+       </main>
     );
 }
 
